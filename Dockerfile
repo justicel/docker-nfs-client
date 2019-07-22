5b6612bd5e18afafdd16fcca617bb906de9ef680
Justice London <justice.m.london@disney.com>
2019-07-22 11:48:24 -0700

    Fix entrypoint to not immediately crash if there are no NFS volumes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM       alpine:latest
+FROM alpine:latest
 
 # USAGE
 # $ docker build -t nfs-client .
--- entry.sh
+++ entry.sh
@@ -1,14 +1,15 @@
 #! /bin/sh -e
 
-mkdir -p "$MOUNTPOINT"
-
-if [$SERVER == ""]; then
-rpc.statd & rpcbind -f & echo "docker NFS client with rpcbind ENABLED... if you wish to mount the mountpoint in this container USE THE FOLLOWING SYNTAX instead:
-$ docker run -itd --privileged=true --net=host -v vol:/mnt/nfs-1:shared -e SERVER= X.X.X.X -e SHARE=shared_path d3fk/nfs-client" & more
-else
-rpc.statd & rpcbind -f &
-mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
+if [ ! -z "$MOUNTPOINT" ]; then
+  mkdir -p "$MOUNTPOINT"
 fi
-mount | grep nfs
+
+if [ -z "$SERVER" ]; then
+  rpc.statd & rpcbind -f & echo "docker NFS client with rpcbind ENABLED... if you wish to mount the mountpoint in this container USE THE FOLLOWING SYNTAX instead:
+  $ docker run -itd --privileged=true --net=host -v vol:/mnt/nfs-1:shared -e SERVER= X.X.X.X -e SHARE=shared_path d3fk/nfs-client"
+else
+  rpc.statd & rpcbind -f &
+  mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
+fi
 
 while true; do sleep 1000; done
